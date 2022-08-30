719771f947261b7882aa35bc79d447420a0fad01
Pritam Desai <32959925+dpritam1986@users.noreply.github.com>
2022-08-30 13:58:29 -0400

    EurekaServer

---
+++ EurekaServer/src/test/java/com/springbootrestapi/MicroService/EurekaServer/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootrestapi.MicroService.EurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
