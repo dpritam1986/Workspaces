027c26bd152de744d35ca4fcfdd701b2137aacfd
Pritam Desai <32959925+dpritam1986@users.noreply.github.com>
2022-08-31 07:34:02 -0400

    State demo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
 
-class RegularClass{}
-class ComponentClass extends Component{}
 
-const regClassInstance = new RegularClass();
-const compClassInstance = new ComponentClass();
-
-console.log('rgg --> ' + regClassInstance);
-console.log('comp -->' + compClassInstance);
 
 
 class App extends Component{
 	
+	constructor(){
+		super();
+		this.state ={ displayBio : false};
+	}
+	
 	render(){
+		
+		let bio = this.state.displayBio ? ( 
+			     <div> 
+			     <p> I live in Sanford Florida</p>
+			     <p> My favorite language is Javascript and Java , and React as front end</p>
+			     <p>Coding is my passion</p>
+			     </div>
+			     ) : null;
+		
 		
 		return (
 			 <div>
 			    <h1>Hello!</h1>
 			    <p>My name is Pritam. I am full stack Java developer having 12+ years of experience. </p>
 			     <p>I am always looking forward to working on challanging projects and thrive to knowledge. </p>
+                 {bio}
 			  </div>
 		)
 	}
