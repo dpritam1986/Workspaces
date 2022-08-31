485e929a9931782926b460872912053bc534eaf6
Pritam Desai <32959925+dpritam1986@users.noreply.github.com>
2022-08-31 18:49:47 -0400

    Added fonts and families and beautification

--- src/Projects.js
+++ src/Projects.js
@@ -9,12 +9,12 @@ class Project extends Component{
 		const{title , image , description , link} = this.props.projectObject;
 		
 		return(		
-			<div style={{display : 'inline-block' , width :300 , margin : 10}}> 
+			<div style={{display : 'inline-block' , width :400 , margin : 10}}> 
 			
               <h3> {title}</h3>
               <img src ={image} alt ='profile' style={{width : 200, height:120}}/>
               <p>{description}</p>
-              <a href={link}>{link} </a>
+              <a href={link} style={{margin :10}}>{link} </a>
                             
          
 		     
