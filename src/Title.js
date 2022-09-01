b50060430711422622b49c2993f1532dd9785efb
Pritam Desai <32959925+dpritam1986@users.noreply.github.com>
2022-09-01 09:48:52 -0400

    Added fading effects in css classes and component

--- src/Title.js
+++ src/Title.js
@@ -11,10 +11,11 @@ const TITLES =[
 
 class Title extends Component{
 	
-	state = {titleIndex : 0};
+	state = {titleIndex : 0 , fadeIn : true};
 	
 	componentDidMount(){
-		console.log('Title comp has mounted');
+		
+	this.timeout=	setTimeout( () => this.setState({fadeIn : false}) , 2000 );
 		
 		this.animateTitles();
 	}
@@ -22,17 +23,19 @@ class Title extends Component{
 	animateTitles = () => {
 		setInterval( () => {
 			const titleIndex= (this.state.titleIndex +1)% TITLES.length;
-	
-			this.setState({titleIndex});
+			this.setState({titleIndex , fadeIn:true});
 			
-		} , 1000 );
+			setTimeout( () => this.setState({fadeIn : false}) , 2000 );
+			
+		} , 3000 );
 	}
 	
 	render(){
+		const {fadeIn , titleIndex} = this.state;
 		
 		const title = TITLES[this.state.titleIndex];
 		
-		return(<p> I am {title}</p>)
+		return(<p className={fadeIn ? 'title-fade-in' : 'title-fade-out'}> I am {title}</p>)
 	}
 	
 }
